85823130f7118356e1b9c9ece76e42f09c47f9a9
Ruslan Sydir <rsydir@gmail.com>
2023-06-14 20:48:34 +0200

    Create DB models

---
+++ models/charger_model.py
@@ -0,0 +1,42 @@
+from app.extensions import db
+from sqlalchemy import DECIMAL
+
+
+class Record(db.Model):
+    __tablename__ = 'record'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    barcode = db.Column(db.Integer(6), unique=True)
+    params_id = db.Column(db.Integer(db.ForeignKey('parameters.id')))
+    datetime = db.Column(db.DateTime)
+
+    parameters = db.relationship('Parameters', backref='record')
+
+
+class Parameters(db.Model):
+    __tablename__ = 'parameters'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    temp_id = db.Column(db. Integer(db.ForeignKey('temperature.id')))
+    voltage_id = db.Column(db. Integer(db.ForeignKey('voltage.id')))
+    current_id = db.Column(db. Integer(db.ForeignKey('current.id')))
+
+    temperature = db.relationship('Temperature', backref='parameters')
+    voltage = db.relationship('voltage', backref='parameters')
+    current = db.relationship('current', backref='parameters')
+
+
+class Temperature(db.Model):
+    __tablename__ = 'temperature'
+    id = db.Column(db.Integer, primaty_key=True, autoincrement=True)
+    temperature = db.Column(DECIMAL(precision=4, scale=1))
+
+
+class Voltage(db.Model):
+    __tablename__ = 'voltage'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    voltage = db.Column(DECIMAL(precision=3, scale=2))
+
+
+class Current(db.Model):
+    __tablename__ = 'current'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    current = db.Column(db.Integer)
